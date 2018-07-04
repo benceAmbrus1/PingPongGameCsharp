b0fa3e52facf6bbd9a38b733b9409b6deb7bc807
benceAmbrus1 <o.g.bence@gmail.com>
2018-07-04 21:52:17 +0200

    basic ping pong

--- PingPong/Form1.Designer.cs
+++ PingPong/Form1.Designer.cs
@@ -28,13 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(150, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(380, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ultimate Ping Pong";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(295, 259);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "New Game";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label3.Location = new System.Drawing.Point(13, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Back";
+            this.label3.Visible = false;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Red;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 150);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(29, 116);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Blue;
+            this.pictureBox2.Location = new System.Drawing.Point(650, 150);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(29, 116);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            this.pictureBox3.Location = new System.Drawing.Point(321, 214);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(22, 23);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Visible = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(684, 461);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Name = "Form1";
+            this.Text = "Ping Pong ";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
 
--- PingPong/Form1.cs
+++ PingPong/Form1.cs
@@ -12,9 +12,172 @@ namespace PingPong
 {
     public partial class Form1 : Form
     {
+     
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = true;
+            pictureBox1.Visible = true;
+            pictureBox2.Visible = true;
+            pictureBox3.Visible = true;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = false;
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyValue == (char)Keys.W)
+            {
+                if (pictureBox1.Location.Y > 0)
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 30);
+                }
+            }
+
+            if ((e.KeyValue == (char)Keys.S))
+            {
+                if (pictureBox1.Location.Y < 500)
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 30);
+                }
+            }
+
+            if (e.KeyValue == (char)Keys.Up)
+            {
+                if (pictureBox2.Location.Y > 0)
+                {
+                    pictureBox2.Location = new Point(pictureBox2.Location.X, pictureBox2.Location.Y - 30);
+                }
+            }
+
+            if ((e.KeyValue == (char)Keys.Down))
+            {
+                if (pictureBox2.Location.Y < 500)
+                {
+                    pictureBox2.Location = new Point(pictureBox2.Location.X, pictureBox2.Location.Y + 30);
+                }
+            }
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread myThread = new System.Threading.Thread(new System.Threading.ThreadStart(BallMove));
+            myThread.Start();
+        }
+
+        private void BallMove()
+            {
+            int ballPower = 10;
+            int ballAngle = 10;
+            int countPasses = 0;
+            while (true)
+            {
+                int wayLeft = new Random().Next(1, 3);
+
+                while (true)
+                {
+                    System.Threading.Thread.Sleep(50);
+                    if(wayLeft == 1)
+                    {
+                        pictureBox3.Location = new Point(pictureBox3.Location.X - ballPower, pictureBox3.Location.Y + new Random().Next(1, ballAngle));
+                        if (pictureBox3.Location.Y - pictureBox3.Height >= 415)
+                        {
+                            wayLeft = 2;
+                        }
+                    }
+                    else
+                    {
+                        pictureBox3.Location = new Point(pictureBox3.Location.X - ballPower, pictureBox3.Location.Y - new Random().Next(1, ballAngle));
+                        if (pictureBox3.Location.Y <= 0)
+                        {
+                            wayLeft = 1;
+                        }
+                    }
+                    if (pictureBox3.Location.X <= 0)
+                    {
+                        label1.Visible = true;
+                        label1.Text = "Game OVER";
+                        label2.Visible = true;
+                        label2.Text = "Right Player Win";
+                        label3.Visible = false;
+                        pictureBox1.Visible = false;
+                        pictureBox2.Visible = false;
+                        pictureBox3.Visible = false;
+                    }
+
+                    if (pictureBox3.Bounds.IntersectsWith(pictureBox1.Bounds))
+                    {
+                        countPasses += 1;
+                        if(countPasses == 5)
+                        {
+                            countPasses = 0;
+                            ballPower += 5;
+                            ballAngle += 2;
+                        }
+                        int wayRight = new Random().Next(1, 3);
+                        while (true)
+                        {
+                            System.Threading.Thread.Sleep(50);
+                            if (wayRight == 1)
+                            {
+                                pictureBox3.Location = new Point(pictureBox3.Location.X + ballPower, pictureBox3.Location.Y+ new Random().Next(1, ballAngle));
+                                if (pictureBox3.Location.Y - pictureBox3.Height >= 415)
+                                {
+                                    wayRight = 2;
+                                }
+                            }
+                            else
+                            {
+                                pictureBox3.Location = new Point(pictureBox3.Location.X + ballPower, pictureBox3.Location.Y- new Random().Next(1, ballAngle));
+                                if (pictureBox3.Location.Y  <= 0)
+                                {
+                                    wayRight = 1;
+                                }
+                            }
+                            
+                            if (pictureBox3.Bounds.IntersectsWith(pictureBox2.Bounds))
+                            {
+                                countPasses += 1;
+                                if (countPasses == 5)
+                                {
+                                    countPasses = 0;
+                                    ballPower += 5;
+                                    ballAngle += 2;
+                                }
+                                break;
+                            }
+                            
+                            if (pictureBox3.Location.X + pictureBox3.Width >= this.Width)
+                            {
+                                label1.Visible = true;
+                                label1.Text = "Game OVER";
+                                label2.Visible = true;
+                                label2.Text = "Left Player Win";
+                                label3.Visible = false;
+                                pictureBox1.Visible = false;
+                                pictureBox2.Visible = false;
+                                pictureBox3.Visible = false;
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
         }
     }
 }
